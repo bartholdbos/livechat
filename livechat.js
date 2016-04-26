88f7baa81d604ab65bd4a7d8579ed6b2e278965d
Barthold Bos <bartholdbos@hotmail.com>
2016-04-27 00:14:32 +0200

    Initial source commit

---
+++ livechat.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Auto N/A
+// @namespace    https://github.com/bartholdbos/livechat
+// @version      0.1
+// @description  Automatic Non Accepting on end of shift
+// @author       Barthold Bos
+// @match        https://my.livechatinc.com/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/mozilla-comm/ical.js/master/build/ical.min.js
+// @downloadURL  https://raw.githubusercontent.com/bartholdbos/livechat/master/livechat.js
+// ==/UserScript==
+
+$(window).load(function(){
+    $.get("https://xss.bos.cloud/", parse);
+    $("#menu ul").append('<li><a id="autona">Auto NA</a></li>');
+    $("#autona").onClick();
+});
+
+function parse(data, textStatus, jqXHR ){
+    var calendar = ICAL.parse(data);
+    var calendarcomp = new ICAL.Component(calendar);
+    var event = calendarcomp.getAllSubcomponents('vevent');
+    var property = event[2].getFirstPropertyValue("dtend");
+    autona(property);
+}
+
+function autona(property){
+    var time = new Date(ICAL.Time.fromData(property).toUnixTime()*1000);
+    var now = new Date();
+    var timetillNA = time - now;
+    if(timetillNA >= 0){
+        setTimeout(function(){$("#status-not-accepting").click(); $("#autona").text("einde shift");}, timetillNA);
+        $("#autona").text("na om: " + time.getHours() + ":" + time.getMinutes());
+    }else{
+        $("#autona").text("einde shift");
+    }
+}
